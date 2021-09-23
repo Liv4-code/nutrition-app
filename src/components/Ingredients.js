c225c2f39188c552dd8b8767b3de63ae5c554312
livi <scholtz.ice@gmail.com>
2021-09-23 19:29:04 +0200

    Got reference to span containing calorie information.

--- src/components/Ingredients.js
+++ src/components/Ingredients.js
@@ -1,21 +1,39 @@
 import React from 'react';
 import TotalCalories from './TotalCalories';
 
-const Ingredients = (props) => {
+class Ingredients extends React.Component {
 
-    const newIngredient = () => props.amount.map(amount => {
+  constructor(props) {
+    super(props);
+
+    this.itemCalRef = React.createRef();
+  }
+
+  componentDidUpdate = () => {
+    console.log(this.itemCalRef);
+  }
+
+  //Get a reference to each list-group-item using the ref system
+  //Receive whatever value that is returned from newIngredient function and append
+  // it to an array in state.
+  // Pass the state down to TotalCalories as a prop.
+
+  render(){
+
+    const newIngredient = () => this.props.amount.map(amount => {
       return <li className="list-group-item d-flex justify-content-between align-items-center">
       {JSON.stringify(amount.item_name)}
-      <span className="badge bg-primary rounded-pill">{amount.nf_calories}</span>
+      <span ref={this.itemCalRef} className="badge bg-primary rounded-pill">{amount.nf_calories}</span>
       </li>
     });
 
   return(
       <div className="list-items">
-        {props.amount.length !== 0 ? newIngredient() : null}
-        <TotalCalories />
+        {this.props.amount.length !== 0 ? newIngredient() : null}
+        <TotalCalories total={this.props.amount.length}/>
       </div>
     )
+  }
 }
 
 export default Ingredients;
--- src/components/TotalCalories.js
+++ src/components/TotalCalories.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
-const TotalCalories = () => {
+const TotalCalories = (props) => {
 
     return(
         <div style={{fontSize: '20px', padding: '20px'}}>
             <label htmlFor="total-calories">Total Calories:</label>
-            <output name="total-calories" htmlFor="total-calories"></output>
+            <output name="total-calories" htmlFor="total-calories">{props.total}</output>
         </div>
     )
 }
